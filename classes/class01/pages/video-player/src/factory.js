6e40ecc4b8606d558d95f83eb920d3cb309b875f
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2023-03-01 15:39:35 -0300

    Worker para funcionar no firefox

--- classes/class01/pages/video-player/src/factory.js
+++ classes/class01/pages/video-player/src/factory.js
@@ -2,6 +2,17 @@ import Controller from "./controller.js"
 import Service from "./service.js"
 import View from "./view.js"
 import Camera from "../../../lib/shared/camera.js"
+import { supportsWorkerType } from "../../../lib/shared/util.js"
+
+async function getWoker(){
+  if(supportsWorkerType()){
+    console.log('suporta');
+    return;
+  }
+
+    console.log('não suporta');
+  
+}
 
 const camera = await Camera.init()
 const [rootPath] = window.location.href.split('/pages/')
