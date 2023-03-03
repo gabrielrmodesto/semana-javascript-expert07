3e322a60e1881a70e8711c1f541d8f3568e2aa98
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2023-03-02 23:43:29 -0300

    Reconhecimento da mão

--- classes/class02/pages/titles/src/services/handGestureService.js
+++ classes/class02/pages/titles/src/services/handGestureService.js
@@ -1,3 +1,36 @@
 export default class HandGestureService{
+  #fingerpose
+  #handPoseDetection
+  #handsVersion
+  #detector = null
+  
+  constructor({ fingerpose, handPoseDetection, handsVersion }){
+    this.#fingerpose = fingerpose
+    this.#handPoseDetection = handPoseDetection
+    this.#handsVersion = handsVersion
+  }
 
+  async estimateHands(video){
+    return this.#detector.estimateHands(video, {
+      flipHorizontal: true
+    })
+  }
+
+  async initializeDetector(){
+    if(this.#detector) return this.#detector
+
+    const detectorConfig = {
+      runtime: 'mediapipe', // or 'tfjs',
+      solutionPath: `https://cdn.jsdelivr.net/npm/@mediapipe/hands@${this.#handsVersion}`,
+      //full mais pesado e mais preciso
+      modelType: 'lite',
+      maxHands: 2,
+    }
+    this.#detector = await this.#handPoseDetection.createDetector(
+      this.#handPoseDetection.SupportedModels.MediaPipeHands,
+      detectorConfig
+    );
+
+    return this.#detector
+  }
 }
